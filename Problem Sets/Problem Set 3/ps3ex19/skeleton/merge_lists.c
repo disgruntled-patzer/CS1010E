076de561a6b20ac586e0f8472b824c49ddbdd0c0
disgruntled-patzer <sps08.lauyanhan@gmail.com>
2018-03-25 15:59:10 +0800

    Modified sorting algorithm for Problem Set 3 Exercise 19
    The original Bubble-Sort algorithm in Problem Set 3 Exercise 19 has been
    replaced with a more efficient double-array sorting process, similar to
    the final phase of the Merge-Sort algorithm. This eliminates the need
    for nested loops and shortens runtime

--- Problem Sets/Problem Set 3/ps3ex19/skeleton/merge_lists.c
+++ Problem Sets/Problem Set 3/ps3ex19/skeleton/merge_lists.c
@@ -53,34 +53,37 @@ void read_list(int list[], int size) {
 void merge_lists(int list1[], int size1,
 		int list2[], int size2, int list3[]) {
 
-    int b; //Variables used within the loops
-    int swapped, sort_temp; //Used for the sorting algorithm.
-    //'swapped' checks whether a swap of digits has taken place. sort_temp is a temporary digit holder used when swapping digits
+    int b1 = 0, b2 = 0, b3 = 0;
+    //In subsequent loop, b1 = index for list1, b2 = index for list2, b3 = index for list3
 
-    for (b = 0; b < size1; b++){
-        list3[b] = list1[b]; //Copy list1 into list3
-    }
-    //print_list(list3,size1); //Debugging statement
-
-    for (b = size1; b < size1 + size2; b++){
-        list3[b] = list2[b - size1]; //Copy list2 into list3, place list2 numbers after list1
-        //Note: it's list [b - size1] not list[b], since writing list[b] will run off the end of the array!
-    }
-    //print_list(list3,size1+size2); //Debugging statement
-
-    do { //Bubble sort algorithm
-        swapped = 0; //Reset swapped to 0; at the start of each iteration, no digits have been swapped
-        for (b = 1; b < size1 + size2; b++){
-            if (list3[b-1] > list3[b]){
-                sort_temp = list3[b-1];
-                list3[b-1] = list3[b];
-                list3[b] = sort_temp;
-                swapped = 1;
-                //If b-1 entry is more than b entry, swap the digits and indicate that a swap has taken place
-            }
+    while (b1 < size1 && b2 < size2){ //While counters for list1 and list2 have not reached their limits
+        if (list1[b1] <= list2[b2]){
+            list3[b3] = list1[b1];
+            b1++; //If list1 index is smaller/equal to list2 index, assign that index to list3 and move on to the next index in list1
         }
+        else {
+            list3[b3] = list2[b2];
+            b2++; //If list2 index is bigger, assign it to list3 and move on to the next index in list2
+        }
+        b3++; //Regardless of whether list1 or list2 index is assigned, list3 moves on to its next index
     }
-    while (swapped == 1); //If no swap has taken place, the list is sorted and the loop exits
+
+    /*In the event that sizes of list1 and list2 are different, there will be extra digits left over in the larger list
+    after the loop has terminated (since it terminates when the max size of the smaller list has been reached
+    Thus, the next two loops are required in order to reassign any remaining elements into list3*/
+
+    while (b1 < size1){ //Assigns any remaining elements from list1 into list3
+        list3[b3] = list1[b1];
+        b1++;
+        b3++;
+    }
+
+    while (b2 < size2){ //Assigns any remaining elements from list2 into list3
+        list3[b3] = list2[b2];
+        b2++;
+        b3++;
+    }
+
 }
 
 // Print values of a list
@@ -93,26 +96,9 @@ void print_list(int list[], int size) {
 	for (c = 0; c < size; c++){
         printf ("%d", list[c]);
         if (c == size - 1){
-            printf ("]\n"); //For the last element, don't print the comma and print a ] instead
+            printf ("]\n"); //For the last index, don't print the comma and print a ] instead
             break;
         }
         printf (", ");
 	}
 }
-
-/*Queries:
-1. For the bubble-sort algorithm, right now the 2 lists are arranged back-to-back and in ascending order each
-This makes the algorithm go through quite a lot of 'useless' steps (checking two numbers but not swapping them)
-in each iteration, but still requires a lot of iterations overall. (TLDR It will take a long time)
-
-Is it possible to shorten this process? I considered something like assigning list1 digits to odd numbered slots in list3,
-and list2 digits to even-numbered slots in list3 (basically the digits in both lists alternate along each other along list 3)
-But this may have problems if the number of digits in list1 and list2 are not the same, thus I did not try it.
-
-2. The problem set requirements suggest that there is a way to do this without nested loops.
-However, most of the sorting algorithms that I have seen, require at least 2 nested loops;
-Those that don't need (e.g. Quick Sort: Divide and Conquer) have quite complicated implementations
-Thus I stuck to Bubble Sort to keep things simple.
-Is is possible to create a simple algorithm that requires no nested loops?
-But then again, I did this exercise before Lecture 8 so there might be something in the lecture that helps
-*/
